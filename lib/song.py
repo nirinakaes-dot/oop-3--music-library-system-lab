ee860fda00ecbc33acdde1c0f3b557c68a21a7cc
Nina Adora <nirinakaes@gmail.com>
2026-06-08 15:50:50 +0300

    Implement Song class with attributes and methods for tracking songs, genres, and artists

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,50 @@
 class Song:
-    pass
+    count =0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    
+    def __init__(self,name,artist,genre):
+         if genre not in Song.genres:
+             Song.genres.append(genre)
+         if artist not in Song.artists:
+             Song.artists.append(artist)
+         if genre not in Song.genre_count:
+             Song.genre_count[genre] =1
+         else:
+             Song.genre_count[genre] +=1
+
+         if artist not in Song.artist_count:
+             Song.artist_count[artist] =1
+         else:
+             Song.artist_count[artist] +=1
+
+
+         
+         self.name = name
+         self.artist = artist
+         self.genre = genre
+         Song.count += 1
+
+    def add_song_to_count(self,count):
+        pass
+       
+
+
+    def add_to_genres(self,genres):
+        pass
+    
+    
+    def add_to_artists(self,artists):
+        if artists not in self.artist:
+            self.artist.append(artists)
+
+
+    def add_to_genre_count(self,genre_count):
+        pass
+
+
+    def add_to_artists_count(self,artists_count):
+        pass
